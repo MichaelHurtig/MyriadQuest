bf26c958fd7abb2a1170eb32a463a23dbc161612
Michael Hurtig <michael.v.hurtig@gmail.com>
2014-09-11 11:52:43 -0600

    New 'Quests' class
    Implemented 'Quests' class to contain quest information. need to make a
    custom adapter that uses 'Quests' to generate list items.

--- app/src/main/java/michaelhurtig/myriadquest/LoginActivity.java
+++ app/src/main/java/michaelhurtig/myriadquest/LoginActivity.java
@@ -15,6 +15,8 @@ public class LoginActivity extends ActionBarActivity {
     EditText username;
     EditText password;
 
+    Quests test = new Quests("first test", "NG", "This is a test string");
+
     // A fairly terrible login method... no security
     public void login(View view){
         String logName = username.getText().toString();
---
+++ app/src/main/java/michaelhurtig/myriadquest/Quests.java
@@ -0,0 +1,74 @@
+package michaelhurtig.myriadquest;
+
+/**
+ * Created by Michael Hurtig on 9/11/2014.
+ */
+public class Quests {
+
+    private String title;
+    private String alignment;
+    private String questText;
+
+    private enum alignments { LG, LN, LE, NG, NN, NE, CG, CN, CE }
+
+    public Quests(String title, String alignment, String questText){
+        this.setTitle(title);
+        this.setAlignment(alignment);
+        this.setQuestText(questText);
+    }
+
+    public String Title(){
+        return title;
+    }
+
+    public String Alignment(){
+        return alignment;
+    }
+
+    public String QuestText(){
+        return questText;
+    }
+
+    public boolean setTitle(String string){
+        try{
+            this.title = string;
+            return true;
+        }
+        catch (Exception exc ){
+            return false;
+        }
+
+    }
+
+    public boolean setAlignment(String string){
+        try{
+            alignments al = alignments.valueOf(string);
+            switch (al){
+                case LG: this.alignment = string; break;
+                case LN: this.alignment = string; break;
+                case LE: this.alignment = string; break;
+                case NG: this.alignment = string; break;
+                case NN: this.alignment = string; break;
+                case NE: this.alignment = string; break;
+                case CG: this.alignment = string; break;
+                case CN: this.alignment = string; break;
+                case CE: this.alignment = string; break;
+                default: return false;
+            }
+            return true;
+        }
+        catch (Exception exc){
+            return false;
+        }
+    }
+
+    public boolean setQuestText(String string){
+        try{
+            this.questText = string;
+            return true;
+        }
+        catch(Exception exc){
+            return false;
+        }
+    }
+}
